afd0755b81bd81f30377a2d58b41f1b9236d2cdc
raq0 <122687779+raq0@users.noreply.github.com>
2023-11-24 16:44:18 +0100

    renamed services

--- resources/views/services.blade.php
+++ resources/views/services.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Raqo | Projects</title>
+    <title>Raqo | Services</title>
     <link rel="shortcut icon" href="media/favicon.png" type="image/x-icon">
     <script src="https://cdn.tailwindcss.com"></script>
 </head>
